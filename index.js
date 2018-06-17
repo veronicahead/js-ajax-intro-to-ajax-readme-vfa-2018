fa99981e80d2fca38c0dc03f6c28812204726b6a
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-17 18:52:30 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function showRepositories(event, data) {
+  //this is set to the XMLHttpRequest object that fired the event
+  console.log(this.responseText)
+}
+ 
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
