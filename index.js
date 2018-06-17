a03ccd0a03899e60050e72d18eece5176b7e5146
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-17 18:53:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function getRepositories() {
   req.send()
 }
 
+function showRepositories(event, data) {
+  console.log(this.responseText)
+  let repoList = "<ul>"
+  for(var i=0;i < this.responseText.length; i++) {
+    repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  }
+  repoList += "</ul>"
+  document.getElementById("repositories").innerHTML = repoList
+}
